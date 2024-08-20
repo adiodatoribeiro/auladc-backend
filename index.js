2d2caf44dc761976f310d50bbb12b939bbdcb026
Adiodato Ribeiro <adiodato6087@DIGITALCOLLEGE.LOCAL>
2024-08-19 21:38:12 -0300

    segundo commit

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const http = require('http');
 
 const PORT = 3000;
 
-const requestHandler = (req, res) => {
-    res.setHeader( 'Content-Type', 'application/json');
+let cars = [
+    { id: 1, brand: 'BMW', model: 'X5', cilinders: '5'},
+    { id: 2, brand: 'Audi', model: 'Q7', cilinders: '7'},
+    { id: 3, brand: 'Mercedes', model: 'GLE', cilinders: '8'},
+]
 
-    if(req.url === '/api' && req.method === 'GET') {
-        const response = { message: 'Hello, World!'}
+const requestHandler = (req, res) => {
+
+    const url = req.url.split('?')[0];
+
+    if(url === '/cars' && req.method === 'GET') {
+        let response;
+
+        if(Boolean(req.url.split('?')[1])) {
+            const idParam = req.url.split('?')[1];
+
+            idToFind = Number(idParam.split("=")[1]);
+
+            response = cars.find(car => car.id === idToFind);
+        } else {
+            response = cars;
+        }
 
         res.writeHead(200);
         res.end(JSON.stringify(response));
 
-    } else if (req.url === "/api" && req.method === "POST"){
+    } else if (req.url === "/cars" && req.method === "POST"){
         let body = '';
 
         req.on('data', chunk =>{
@@ -21,14 +38,23 @@ const requestHandler = (req, res) => {
         req.on('end', () =>{
             const data = JSON.parse(body);
 
-            const response = {
-                message: 'Data received successfully',
-                data: data,
-                timestamp: new Date().toISOString()
-            };
+            if(!data?.brand || !data?.model || !data?.cilinders) {
+                res.writeHead(400);
+                res.end(JSON.stringify({ message: 'Bad request'}));
+                return;
+            }
 
-            res.writeHead(200);
-            res.end(JSON.stringify(response));
+            const newCar = {
+                id: cars.length + 1,
+                brand: data.brand,
+                model: data.model,
+                cilinders: data.cilinders
+            }
+
+            cars.push(newCar);
+
+            res.writeHead(201);
+            res.end(JSON.stringify(newCar));
         });
 
     } else {
